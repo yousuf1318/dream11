3d964c66b5c7572981dab45f7bad8eb15fcc418f
yousuf1318 <61650368+yousuf1318@users.noreply.github.com>
2022-06-01 11:11:59 +0530

    Add files via upload

---
+++ public/app/register.js
@@ -0,0 +1,54 @@
+$(() => {
+
+    $('#loginn').click(()=>{
+        console.log('login clicked')
+        $('#content').load('html/login.html')
+    })
+    $('#facebook').click(()=>{
+        console.log('facebook')
+        window.open("https://dream786.herokuapp.com/f/login","_self");
+        // $.post('/f/login',(data)=>{
+        //     console.log(data);
+        // })
+
+    })
+    $('#signin').click(() => {
+        let password = $('#password').val();
+        let p1 = $('#password1').val();
+        if (password != p1) {
+            window.alert('Password Not match');
+        }
+        else {
+            let name = $('#first').val() + ' ' + $('#last').val();
+            let email = $('#email').val();
+            let username = $('#username').val();
+            let phone = $('#phone').val();
+            $.get(`/user/${username}`,(data)=>{
+                if(!(Object.keys(data).length === 0)){
+                    window.alert('This username alredy exits')
+                }
+                else{
+                    $.post('/user',{
+                        name,email,password,phone,username
+                    },(data)=>{
+                        console.log(data)
+                        window.alert(`The account for ${data.username} is created`)
+                        $.post('/s/t',{
+                            total:0,
+                            user:data.id
+                        },()=>{
+                            console.log('done')
+                        })
+                        $('#content').load('html/dashboard.html')
+                    })
+                }
+            })
+            
+        }
+
+
+        console.log('signin clicked')
+    })
+
+    
+})
